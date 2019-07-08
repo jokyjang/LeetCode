a61d767f7669353193e27bd9efa5f2506cbaa637
Zhaoxi Zhang <zhangzhx1989@gmail.com>
2019-07-07 23:35:41 -0700

    Add number addition with linked list in normal order.

---
+++ medium/src/main/java/Solution445.java
@@ -0,0 +1,54 @@
+import java.util.Stack;
+
+public class Solution445 {
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = pushToStack(l1);
+        Stack<Integer> s2 = pushToStack(l2);
+
+        int carry = 0;
+        ListNode lowDigit = null;
+        while (!s1.isEmpty() || !s2.isEmpty()) {
+            int sum = carry;
+            if (!s1.isEmpty()) {
+                sum += s1.pop();
+            }
+            if (!s2.isEmpty()) {
+                sum += s2.pop();
+            }
+            ListNode newNode = new ListNode(sum % 10);
+            newNode.next = lowDigit;
+            lowDigit = newNode;
+            carry = sum / 10;
+        }
+
+        if (carry != 0) {
+            ListNode newNode = new ListNode(carry);
+            newNode.next = lowDigit;
+            lowDigit = newNode;
+        }
+
+        return lowDigit;
+    }
+
+    // Push the linked list to stack
+    private Stack<Integer> pushToStack(ListNode node) {
+        Stack<Integer> stack = new Stack<>();
+
+        while (node != null) {
+            stack.push(node.val);
+            node = node.next;
+        }
+
+        return stack;
+    }
+}
