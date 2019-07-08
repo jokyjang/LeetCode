a47cc80ad7a876be16e62594dd20dd64762788e6
Zhaoxi Zhang <zhangzhx1989@gmail.com>
2019-07-07 23:00:44 -0700

    Add two number sum from reversed linked list with each digit in a node.

---
+++ medium/src/main/java/Solution2.java
@@ -0,0 +1,42 @@
+public class Solution2 {
+
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode root = null;
+        ListNode current = null;
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            ListNode newNode = new ListNode(sum % 10);
+            if (root == null) {
+                root = newNode;
+            } else {
+                current.next = newNode;
+            }
+            current = newNode;
+        }
+
+        if (carry != 0) {
+            current.next = new ListNode(carry);
+        }
+        return root;
+    }
+}
---
+++ medium/src/test/java/Solution2Test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class Solution2Test {
+
+    /**
+     * TODO add test
+     */
+    @Test
+    public void addTwoNumbers() {
+//        Solution2.ListNode sum = new Solution2().addTwoNumbers(l1, l2);
+    }
+}
