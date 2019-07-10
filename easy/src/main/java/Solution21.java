eac3622221462285f5182a522aaed7b8077fd47a
Zhaoxi Zhang <zhangzhx1989@gmail.com>
2019-07-10 11:01:18 -0700

    Merge two sorted linked list.

---
+++ easy/src/main/java/Solution21.java
@@ -0,0 +1,36 @@
+public class Solution21 {
+
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode root = null;
+        ListNode current = null;
+        while (l1 != null || l2 != null) {
+            ListNode node = null;
+            if (l1 == null || (l2 != null && l1.val > l2.val)) {
+                node = new ListNode(l2.val);
+                l2 = l2.next;
+            } else {
+                node = new ListNode(l1.val);
+                l1 = l1.next;
+            }
+            if (root == null) {
+                root = node;
+
+            } else {
+                current.next = node;
+
+            }
+            current = node;
+        }
+        return root;
+    }
+}
---
+++ easy/src/test/java/Solution21Test.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class Solution21Test {
+
+    /**
+     * TODO add test for this class
+     */
+    @Test
+    public void mergeTwoLists() {
+
+    }
+
+}
