fd4dce6eb2cee9395dbddb7b1ecdf3ecead82ecb
Zhaoxi Zhang <zhangzhx1989@gmail.com>
2019-07-21 13:59:01 -0700

    Add preorder traversal of tree

---
+++ medium/src/main/java/Solution144.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ medium/src/test/java/Solution144Test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class Solution144Test {
+
+    @Test
+    public void preorder() {
+        Solution144 solution = new Solution144();
+
+        // TODO add TreeNode tests
+    }
+
+}
