e21e86e54b4700aaac3d5f5b59db3911152abd3a
Zhaoxi Zhang <zhangzhx1989@gmail.com>
2019-07-21 17:33:34 -0700

    Add max product subarray

--- medium/src/main/java/Solution152.java
+++ medium/src/main/java/Solution152.java
@@ -1,16 +1,49 @@
-public class Solution152 {
-    public int maxProduct(int[] nums) {
-        int[] products = new int[nums.length];
-        int max = nums[0];
-        for (int i = 0; i < nums.length; ++i) {
-            products[i] = nums[i];
-            max = Math.max(max, products[i]);
-            for (int j = 0; j < i; ++j) {
-                products[j] = products[j] * nums[i];
-                max = Math.max(max, products[j]);
-            }
-        }
+public interface Solution152 {
+    int maxProduct(int[] nums);
 
-        return max;
+    class ON implements Solution152 {
+        @Override
+        public int maxProduct(int[] nums) {
+            int maxPositive = 0, minNegative = 0;
+            int max = nums[0];
+            if (nums[0] > 0) {
+                maxPositive = nums[0];
+            } else if (nums[0] < 0) {
+                minNegative = nums[0];
+            }
+            for (int i = 1; i < nums.length; ++i) {
+                if (nums[i] > 0) {
+                    maxPositive = Math.max(nums[i], nums[i] * maxPositive);
+                    minNegative = nums[i] * minNegative;
+                } else if (nums[i] < 0) {
+                    int temp = maxPositive;
+                    maxPositive = nums[i] * minNegative;
+                    minNegative = Math.min(nums[i], nums[i] * temp);
+                } else {
+                    maxPositive = 0;
+                    minNegative = 0;
+                }
+                max = Math.max(max, maxPositive);
+            }
+            return max;
+        }
+    }
+
+    class ON2 implements Solution152 {
+        @Override
+        public int maxProduct(int[] nums) {
+            int[] products = new int[nums.length];
+            int max = nums[0];
+            for (int i = 0; i < nums.length; ++i) {
+                products[i] = nums[i];
+                max = Math.max(max, products[i]);
+                for (int j = 0; j < i; ++j) {
+                    products[j] = products[j] * nums[i];
+                    max = Math.max(max, products[j]);
+                }
+            }
+
+            return max;
+        }
     }
 }
--- medium/src/test/java/Solution152Test.java
+++ medium/src/test/java/Solution152Test.java
@@ -5,8 +5,15 @@ import static org.junit.Assert.assertEquals;
 public class Solution152Test {
 
     @Test
-    public void maxProduct() {
-        Solution152 solution = new Solution152();
+    public void maxProduct_on() {
+        Solution152 solution = new Solution152.ON();
+        assertEquals(6, solution.maxProduct(new int[]{2, 3, -2, 4}));
+        assertEquals(0, solution.maxProduct(new int[]{-2, 0, -1}));
+    }
+
+    @Test
+    public void maxProduct_on2() {
+        Solution152 solution = new Solution152.ON2();
         assertEquals(6, solution.maxProduct(new int[]{2, 3, -2, 4}));
         assertEquals(0, solution.maxProduct(new int[]{-2, 0, -1}));
     }
