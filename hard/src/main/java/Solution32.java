4c5166a90ab0638510f30a970a9af4c849855f74
Zhaoxi Zhang <zhangzhx1989@gmail.com>
2019-08-03 11:38:06 -0700

    Add another solution for valid parentheses

--- hard/src/main/java/Solution32.java
+++ hard/src/main/java/Solution32.java
@@ -1,25 +1,52 @@
 import java.util.Stack;
 
-public class Solution32 {
-    public int longestValidParentheses(String s) {
-        int[] longest = new int[s.length()];
-        Stack<Integer> stack = new Stack<>();
-        int max = 0;
-        if (s.isEmpty()) {
-            return max;
-        }
-        for (int i = 0; i < s.length(); ++i) {
-            if (s.charAt(i) == '(') {
-                stack.push(i);
-            } else if (!stack.isEmpty()) {
-                int prev = stack.pop();
-                int current = 2 + longest[i-1] + (prev >= 1 ? longest[prev-1] : 0);
-                longest[i] = current;
-                if (current > max) {
-                    max = current;
+public interface Solution32 {
+
+    int longestValidParentheses(String s);
+
+    class DP implements Solution32 {
+        @Override
+        public int longestValidParentheses(String s) {
+            int[] longest = new int[s.length()];
+            int max = 0;
+            for (int i = 1; i < s.length(); ++i) {
+                if (s.charAt(i) == ')') {
+                    int prev = i - longest[i-1] - 1;
+                    if (prev >= 0 && s.charAt(prev) == '(') {
+                        longest[i] = 2 + longest[i-1];
+                        if (prev - 1 >= 0) {
+                            longest[i] += longest[prev-1];
+                        }
+                        max = Math.max(max, longest[i]);
+                    }
                 }
             }
+            return max;
         }
-        return max;
+    }
+
+    class StackSolution implements Solution32 {
+        @Override
+        public int longestValidParentheses(String s) {
+            int[] longest = new int[s.length()];
+            Stack<Integer> stack = new Stack<>();
+            int max = 0;
+            if (s.isEmpty()) {
+                return max;
+            }
+            for (int i = 0; i < s.length(); ++i) {
+                if (s.charAt(i) == '(') {
+                    stack.push(i);
+                } else if (!stack.isEmpty()) {
+                    int prev = stack.pop();
+                    int current = 2 + longest[i-1] + (prev >= 1 ? longest[prev-1] : 0);
+                    longest[i] = current;
+                    if (current > max) {
+                        max = current;
+                    }
+                }
+            }
+            return max;
+        }
     }
 }
--- hard/src/test/java/Solution32Test.java
+++ hard/src/test/java/Solution32Test.java
@@ -5,10 +5,14 @@ import static org.junit.Assert.assertEquals;
 public class Solution32Test {
     @Test
     public void longestValidParentheses() {
-        Solution32 solution = new Solution32();
+        Solution32 solution = new Solution32.StackSolution();
+        assertEquals(2, solution.longestValidParentheses("(()"));
+        assertEquals(4, solution.longestValidParentheses(")()())"));
+        assertEquals(2, solution.longestValidParentheses("()(()"));
+
+        solution = new Solution32.DP();
         assertEquals(2, solution.longestValidParentheses("(()"));
         assertEquals(4, solution.longestValidParentheses(")()())"));
         assertEquals(2, solution.longestValidParentheses("()(()"));
     }
-
 }
