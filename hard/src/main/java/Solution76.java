ecf76ff6cf2c09b22e73e79956b3b0cc26dbaf40
Zhaoxi Zhang <zhangzhx1989@gmail.com>
2019-08-02 21:47:41 -0700

    Add minimum window substring

--- hard/src/main/java/Solution76.java
+++ hard/src/main/java/Solution76.java
@@ -1,3 +1,37 @@
 public class Solution76 {
+    public String minWindow(String s, String t) {
+        int[] set = new int[256];
+        for (char c : t.toCharArray()) {
+            set[c]++;
+        }
 
+        int low = 0, high = 0, min = Integer.MAX_VALUE, start = low, counter = t.length();
+        while (high < s.length()) {
+            if (set[s.charAt(high)] != 0) {
+                if (set[s.charAt(high)] > 0) {
+                    counter--;
+                }
+                set[s.charAt(high)]--;
+                if (set[s.charAt(high)] == 0) {
+                    set[s.charAt(high)] = -1;
+                }
+            }
+            high++;
+            while (counter == 0) {
+                if (high-low < min) {
+                    min = high-low;
+                    start = low;
+                }
+                if (set[s.charAt(low)] < 0) {
+                    set[s.charAt(low)]++;
+                    if (set[s.charAt(low)] == 0) {
+                        counter++;
+                        set[s.charAt(low)] = 1;
+                    }
+                }
+                low++;
+            }
+        }
+        return min == Integer.MAX_VALUE ? "" : s.substring(start, start + min);
+    }
 }
---
+++ hard/src/test/java/Solution76Test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution76Test {
+
+    @Test
+    public void minWindow() {
+        Solution76 solution = new Solution76();
+        assertEquals("BANC", solution.minWindow("ADOBECODEBANC", "ABC"));
+        assertEquals("", solution.minWindow("aa", "a"));
+    }
+
+}
