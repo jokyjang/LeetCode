400fca229d769f80f546cb0ccd68b6301c4f37f6
Zhaoxi Zhang <zhangzhx1989@gmail.com>
2019-08-19 21:18:31 -0700

    Add count prime number

--- easy/src/main/java/Solution204.java
+++ easy/src/main/java/Solution204.java
@@ -1,3 +1,29 @@
+// TODO ADD TESTS
 public class Solution204 {
+    public int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i < n; ++i) {
+            if (isPrime(i)) {
+                ++count;
+            }
+        }
+        return count;
+    }
 
+    private boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        } else if (number == 2 || number == 3) {
+            return true;
+        } else if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        } else {
+            for (int i = 5; i * i <= number; ++i) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
